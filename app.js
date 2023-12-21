e5f59764b71d187ca799e756a909708391287e5a
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2023-12-21 12:27:42 +0100

    Working with Types

---
+++ app.js
@@ -0,0 +1,5 @@
+function addNum(a, b) {
+    return a + b;
+}
+var resultNum = addNum("5", "3");
+console.log(resultNum);
---
+++ app.ts
@@ -0,0 +1,7 @@
+function addNum(a: number, b: number) {
+  return a + b;
+}
+
+const resultNum = addNum(5, 3);
+
+console.log(resultNum);
