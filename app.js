999a0061866a70e81cad0267d76f48c6c78e5e73
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2023-12-21 12:58:20 +0100

    Core Types and Deeper Dive

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+var num1Input = document.getElementById("num1");
+var num2Input = document.getElementById("num2");
+var buttonElement = document.querySelector("button");
 function addNum(a, b) {
     return a + b;
 }
-var resultNum = addNum("5", "3");
+function printResult(result) {
+    console.log(result);
+}
+var resultNum = addNum(5, 3);
+var isDone = false;
 console.log(resultNum);
+printResult(resultNum);
+buttonElement.addEventListener("click", function () {
+    var num1 = +num1Input.value;
+    var num2 = +num2Input.value;
+    var result = addNum(num1, num2);
+    printResult(result);
+});
--- app.ts
+++ app.ts
@@ -1,7 +1,25 @@
-function addNum(a: number, b: number) {
+const num1Input = document.getElementById("num1") as HTMLInputElement;
+const num2Input = document.getElementById("num2") as HTMLInputElement;
+const buttonElement = document.querySelector("button") as HTMLButtonElement;
+
+function addNum(a: number, b: number): number {
   return a + b;
 }
 
+function printResult(result: any): void {
+  console.log(result);
+}
+
 const resultNum = addNum(5, 3);
+let isDone: boolean = false;
 
 console.log(resultNum);
+
+printResult(resultNum);
+
+buttonElement.addEventListener("click", () => {
+  const num1 = +num1Input.value;
+  const num2 = +num2Input.value;
+  const result = addNum(num1, num2);
+  printResult(result);
+});
